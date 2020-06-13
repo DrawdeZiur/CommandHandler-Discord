808b252ba3d7f87872aea9445d9694a8feb6f15f
DrawdeZiur <60681365+DrawdeZiur@users.noreply.github.com>
2020-06-13 13:53:51 -0400

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,15 @@
+module.exports = (client, message) => { 
+
+  if(!message.content.startsWith(client.config.prefix)) return; 
+  if(message.author.bot) return;
+
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);  
+  const command = args.shift().toLowerCase()
+
+  let cmd = client.commands.get(command);
+  if(!cmd) return;
+
+  cmd(client, message, args);
+  
+
+}
